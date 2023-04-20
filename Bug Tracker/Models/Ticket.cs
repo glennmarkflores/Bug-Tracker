6c878d34d8956d91d6062194d8442989a73a2c9f
GLENN MARK FLORES <floresglennmark546@gmail.com>
2023-04-20 16:59:49 +0800

    feat: Add Models needed for Ticket feature

---
+++ Bug Tracker/Models/Ticket.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Bug_Tracker.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        [DisplayName("Title")]
+        public string Title { get; set; }
+
+        [Required]
+        [DisplayName("Description")]
+        public string Desciption { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Created")]
+        public DateTimeOffset Created { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayName("Updated")]
+        public DateTimeOffset? Updated { get; set; }
+
+        [DisplayName("Archived")]
+        public bool Archied { get; set; }
+
+        [DisplayName("Project")]
+        public int ProjectId { get; set; }
+
+        [DisplayName("Ticket Type")]
+        public int TicketTypeId { get; set; }
+
+        [DisplayName("Ticket Priority")]
+        public int TicketPriorityId { get; set; }
+
+        [DisplayName("Ticket Status")]
+        public int TicketStatusId { get; set; }
+
+        [DisplayName("Ticket Owner")]
+        public string OwnerUserId { get; set; }
+
+        [DisplayName("Ticket Developer")]
+        public string DeveloperUserId { get; set; }
+
+
+        // Navigational Properties
+        public virtual Project Project { get; set; }
+        
+        public virtual TicketType TicketType { get; set; }
+        
+        public virtual TicketPriority TicketPriority { get; set; }
+        
+        public virtual TicketStatus TicketStatus { get; set; }
+        
+        public virtual BTUser OwnerUser { get; set; }
+        
+        public virtual BTUser DeveloperUser { get; set; }
+
+
+        public virtual ICollection<TicketComment> Comments { get; set; } = new HashSet<TicketComment>();
+
+        public virtual ICollection<TicketAttachment> Attachments { get; set; } = new HashSet<TicketAttachment>();
+
+        public virtual ICollection<Notifications> Notifications { get; set; } = new HashSet<Notifications>();
+
+        public virtual ICollection<TicketHistory> History { get; set; } = new HashSet<TicketHistory>();
+
+    }
+}
---
+++ Bug Tracker/Models/TicketStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel;
+
+namespace Bug_Tracker.Models
+{
+    public class TicketStatus
+    {
+        public int Id { get; set; }
+
+        [DisplayName("Status Name")]
+        public string Name { get; set; }
+
+    }
+}
